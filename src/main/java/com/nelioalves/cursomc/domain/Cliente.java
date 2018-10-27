3804e7ebacc58485e314e1c191cbf84948324d64
Adilson Araujo <adilson.pxef.araujo@gmail.com>
2018-10-27 18:07:53 -0300

    Pedido,EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -33,6 +33,8 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -130,6 +132,14 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 	
